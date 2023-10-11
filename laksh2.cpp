c094877fcc1f4e83c563679f6285c5231f2c95e9
lakshsharda <147646330+lakshsharda@users.noreply.github.com>
2023-10-11 22:39:10 +0530

    Create laksh2.cpp

---
+++ laksh2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+
+   
+    int product = num1 * num2;
+
+    
+    std::cout << "The product of " << num1 << " and " << num2 << " is: " << product << std::endl;
+
+    return 0;
+}
